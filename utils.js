3a6ebe7b456086ecb4688bc17b04554e1b772297
olehhladkov <oleh.hladkov@gmail.com>
2021-11-19 16:28:11 +0200

    --amend

--- utils.js
+++ utils.js
@@ -8,7 +8,7 @@ export const getCategories = async () =>
 export const getQuiz = async ({ category, amount = 1 } = {}) => {
     const url = `${process.env.API_URL_BASE}?amount=${amount}${
         category ? `&category=${category}` : ''
-    }`
+    }&type=multiple`
 
     return await axios.get(url).then(({ data }) => data.results)
 }
