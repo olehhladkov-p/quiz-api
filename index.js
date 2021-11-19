6c45a1bcb95e0bed5ce75573a89d2ba83894db10
olehhladkov <43970746+olehhladkov@users.noreply.github.com>
2021-11-19 16:33:17 +0200

    Add basic routes (#1)
    * Add express app, add healthcheck route
    
    * Add categories route
    
    * Add quiz route
    
    * Add 'start' script
    
    * Update tabWidth

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import { getCategories, getQuiz } from './utils.js'
+
+dotenv.config()
+
+const API_VERSION = process.env.API_VERSION || 'v1'
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get(`/api/${API_VERSION}/healthcheck`, (req, res) => {
+  res.json({ status: 200 })
+})
+
+app.get(`/api/${API_VERSION}/categories`, async (req, res) => {
+  res.json({ status: 200, categories: await getCategories() })
+})
+
+app.get(`/api/${API_VERSION}/quiz`, async (req, res) => {
+  res.json({ status: 200, quiz: await getQuiz(req.query) })
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port: ${port}`)
+})
---
+++ utils.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const getCategories = async () =>
+  await axios
+    .get(process.env.API_URL_CATEGORIES)
+    .then(({ data }) => data.trivia_categories)
+
+export const getQuiz = async ({ category, amount = 1 } = {}) => {
+  const url = `${process.env.API_URL_BASE}?amount=${amount}${
+    category ? `&category=${category}` : ''
+  }&type=multiple`
+
+  return await axios.get(url).then(({ data }) => data.results)
+}
